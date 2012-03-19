9cce2e8e59778b73395596789108e240d184463e
Doug Black <doug@itsdoug.com>
2012-03-18 19:06:51 -0700

    Added miles.py

---
+++ miles.py
@@ -0,0 +1,36 @@
+import sys
+
+def processInput():
+	if (sys.argv[1] == "="):
+		printWeekMiles(sys.argv[2])
+	elif (sys.argv[1] == "+"):
+		addMilage(sys.argv[2], sys.argv[3])
+	else:
+		print "Error: incorrect arguments"
+		print "Format: miles.py [+ or = ] [params...]"
+
+
+def addMilage(milage, week):
+	f = open(".miles.txt", "a+")
+	f.write(str(week) + "|" + str(milage) + "\n")
+	f.close
+
+def printWeekMiles(week):
+	f = open(".miles.txt", "r")
+	line = f.readline()
+	print
+	print"Milage for week: ",week
+	total = 0
+	while (line != ""):
+		lineWeek = list(line.split("|"))
+		milage = lineWeek[1].split("\n")[0]
+		if str(week)==lineWeek[0]:
+			print milage
+			total += int(lineWeek[1].split("\n")[0])
+		line = f.readline()
+	print 
+	print "Total: " + str(total)
+	print
+	f.close()
+		
+processInput()
